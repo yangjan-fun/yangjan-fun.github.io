6b02b666c6d2577a0c2adec6b137dac8789e22ac
yangjan-fun <janneyang0818@gmail.com>
2021-01-06 13:58:44 -0500

    f

--- Previous/Scatterplot/script.js
+++ Previous/Scatterplot/script.js
@@ -89,7 +89,7 @@ d3.csv("./data/happyscore.csv").then(function(data) {
         .attr("class","tooltip");
 
     points.on("mouseover", function(d) {
-        var cx = +d3.select(this).attr("cx"+300);
+        var cx = +d3.select(this).attr("cx"-100);
         var cy = +d3.select(this).attr("cy");
     
     tooltip.style("visibility","visible")
